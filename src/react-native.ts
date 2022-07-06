390e6b3c6dabd6cbf06764037523f1842c0a5e5e
Vincent Vella <vincevella0@gmail.com>
2022-07-05 20:47:19 -0400

    feat: text input tailwind, add email and password

--- src/react-native.ts
+++ src/react-native.ts
@@ -4,9 +4,11 @@ import {
   Text as RNText,
   SafeAreaView as RNSafeAreaView,
   Image as RNImage,
+  TextInput as RNTextInput,
 } from 'react-native';
 
 export const Image = styled(RNImage);
 export const SafeAreaView = styled(RNSafeAreaView);
-export const Text = styled(RNText, 'font-sans');
+export const Text = styled(RNText, 'font-sans text-dark dark:text-light');
+export const TextInput = styled(RNTextInput, 'font-sans text-dark dark:text-light');
 export const View = styled(RNView);
--- src/screens/Landing.tsx
+++ src/screens/Landing.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { View, Text, Image } from '~/react-native';
+import { View, Text, Image, TextInput } from '~/react-native';
 
 const Landing = () => {
   return (
-    <View className="w-full">
-      <Image className="w-32 h-32 self-center" source={require('../../assets/icon.png')} />
-      <Text className="text-light self-center text-5xl">DootTodo</Text>
-      <Text className="text-light self-center text-3xl">Get More Done</Text>
-    </View>
+    <>
+      <View className="w-full">
+        <Image className="w-32 h-32 self-center" source={require('../../assets/icon.png')} />
+        <Text className="self-center text-5xl">DootTodo</Text>
+        <Text className="self-center text-3xl">Get More Done</Text>
+      </View>
+      <View className="p-4">
+        <Text className="text-xl pt-4">Email</Text>
+        <TextInput className="border border-light rounded p-2" placeholder="user@email.com" />
+        <Text className="text-xl pt-4">Password</Text>
+        <TextInput className="border border-light rounded p-2" placeholder="**********" />
+      </View>
+    </>
   );
 };
 
